e829a8c860c8c79f7081a2f8c769be825368cb28
Kostas Georgiadis <kongeor@gmail.com>
2016-12-28 08:54:39 -0500

    Implement flower submission form

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,10 +1,44 @@
+var flower =
+{
+  name: "",
+  description: ""
+};
+
 Vue.component('heading', {
   template: '<h1>Flowers</h1>'
 });
 
+Vue.component('create-flower', {
+  template: '#create-flower-template',
+  data: function() {
+    return flower;
+  },
+  methods: {
+    onSubmit: function() {
+      var that = this;
+      if (!this.name) {
+        alert('name is required!')
+      } else {
+        axios.post('/api/flowers', {
+          name: this.name,
+          description: this.description
+        })
+        .then(function (response) {
+          flower.name = "";
+          flower.description = "";
+          that.$emit('created', response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
+    }
+  }
+});
+
 Vue.component('flower', {
   props: ['flower'],
-  template: '<div><h3>{{ flower.name }}</h3></div>'
+  template: '<div><h3>{{ flower.name }}</h3><p>{{ flower.description }}</p></div>'
 });
 
 Vue.component('flower-list', {
@@ -26,5 +60,10 @@ var app = new Vue({
         console.log('fail: ' + error);
       });
   },
-  template: '<div class="container"><heading /><flower-list v-bind:flowers="flowers"></flower-list></div>'
+  template: '<div class="container"><heading /><create-flower v-on:created="flowerCreated" /><flower-list v-bind:flowers="flowers"></flower-list></div>',
+  methods: {
+    flowerCreated: function(flower) {
+        this.flowers.push(flower);
+    }
+  }
 });
