c1d7bbefb20543c55b67eb9ec6247f46baa05c5d
Kostas Georgiadis <kongeor@gmail.com>
2016-12-29 07:06:34 -0500

    enhance schema

--- src/main/resources/db/migration/V1__Create_initial_schema.sql
+++ src/main/resources/db/migration/V1__Create_initial_schema.sql
@@ -1,14 +1,22 @@
 create table users (
-  id serial,
-  username text,
-  password text,
-  email text
+  id bigserial primary key,
+  username text not null unique,
+  password text not null,
+  email text not null unique
 );
 
 insert into users(username, password, email) values('admin', '$31$16$9VqHXYGMhdWH2Q9cTxwie_E6NsltXJJLOGNn-tw6jtM', 'admin@example.com');
 
 create table flowers (
-  id serial,
-  name text,
+  id bigserial primary key,
+  name text not null unique,
   description text
-)
+);
+
+create table user_flowers (
+  id bigserial,
+  user_id integer references users(id) on delete cascade,
+  flower_id integer references flowers(id) on delete cascade,
+  notext text,
+  primary key(id, user_id, flower_id)
+);
